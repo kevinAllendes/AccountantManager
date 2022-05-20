425102c915f783d417bbc651f15a2e9700e020bd
kevinAllendes <kevinallendes1993@gmail.com>
2022-05-20 13:18:45 -0300

    Initial commit

---
+++ Login_Menu.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace AccountantManager
+{
+    /// <summary>
+    /// Lógica de interacción para Login_Menu.xaml
+    /// </summary>
+    public partial class Login_Menu : Window
+    {
+        //Conexion sql a utilizar mas adelante
+        SqlConnection miConexionSql;
+
+        //Cadena de conexion a utilizar
+        string miConexion = ConfigurationManager.ConnectionStrings["AccountantManager.Properties.Settings.LoginConnectionString"].ConnectionString;
+
+        public Login_Menu()
+        {
+            InitializeComponent();
+
+            
+
+            
+        }
+
+        private void BtnLogin_Click(object sender, RoutedEventArgs e)
+        {
+            if (TBUsuario.Text != "")
+            {
+                string consultaLogin = "SELECT Pass FROM TableLogin WHERE  Nombre="+TBUsuario.Text.ToString();
+
+                using (SqlConnection connect = new SqlConnection(miConexion))
+                {
+                    SqlCommand cmd = new SqlCommand(consultaLogin, connect);
+                    connect.Open();
+
+                    SqlDataReader reader = cmd.ExecuteReader();
+                    MessageBox.Show(reader.GetString(0));
+                    connect.Close();
+    
+                }
+
+            }
+            MessageBox.Show("Debe ingresar algun valor");
+
+
+        }
+    }
+}
