e0dc2975676cf54a13c04ae3e7937c8e9874547f
kevinAllendes <kevinallendes1993@gmail.com>
2022-05-20 21:55:33 -0300

    Update Login_Menu.xaml.cs

--- Login_Menu.xaml.cs
+++ Login_Menu.xaml.cs
@@ -41,6 +41,39 @@ namespace AccountantManager
         {
             /*
             https://rjcodeadvance.com/cap-2-login-logout-y-mostrar-datos-del-usuario-validaciones-con-arquitectura-en-capas-poo-c-y-mysql-nivel-intermedio/
+
+            public bool Login(string user, string pass)
+             {
+               using (var connection = GetConnection())
+                {
+                connection.Open();
+                using (var command = new MySqlCommand())
+                {
+                    command.Connection = connection;
+                    command.CommandText = "select *from users where (loginName=@user and password=@pass) or (Email=@user and password=@pass)";
+                    command.Parameters.AddWithValue("@user", user);
+                    command.Parameters.AddWithValue("@pass", pass);
+                    command.CommandType = CommandType.Text;
+                    MySqlDataReader reader = command.ExecuteReader();
+                    if (reader.HasRows)
+                    {
+                        while (reader.Read())//Obtenemos los datos de la columna y asignamos a los campos de la Cache de Usuario
+                        {
+                            UserCache.IdUser = reader.GetInt32(0); 
+                            UserCache.LoginName = reader.GetString(1);
+                            UserCache.Password = reader.GetString(2);
+            UserCache.FirstName = reader.GetString(3); 
+            UserCache.LastName = reader.GetString(4);
+            UserCache.Position = reader.GetString(5);
+            UserCache.Email = reader.GetString(6);
+                 }
+                      return true;
+                   }
+                   else
+                  return false;
+                   }
+                 }
+            }
             */
             if (TBUsuario.Text != "")
             {
